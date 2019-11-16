79c611ad2d4cb6ac789542c6365aac4716afb3f6
liukailocalhost <578123466@qq.com>
2019-11-17 00:17:43 +0800

    first commit

---
+++ first_day/test_demo/src/main/java/com/example/demo/first_day/test_git.java
@@ -0,0 +1,13 @@
+package com.example.demo.first_day;
+
+/**
+ * Created by Lenovo on 2019/11/16.
+ * 第一天:测试git
+ */
+public class test_git {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
